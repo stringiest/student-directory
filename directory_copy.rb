c2074031b165d3844a35c0cddd7c026ff2b7cc3b
Lucy Stringer <lucyjstringer@gmail.com>
2020-10-21 14:42:42 +0100

    centralise title

--- directory_copy.rb
+++ directory_copy.rb
@@ -18,8 +18,8 @@ def input_students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(80)
+  puts "-------------".center(80)
 end
 
 def print(names)
