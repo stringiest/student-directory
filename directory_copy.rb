2f302d299f4c2c0ce1f5404ba81be6cbe095e303
Lucy Stringer <lucyjstringer@gmail.com>
2020-10-21 11:40:39 +0100

    Student directory with numbers and only beginning with D

---
+++ directory_copy.rb
@@ -0,0 +1,41 @@
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student has to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(names)
+  names.each_with_index do |name, index|
+    if name[:name].start_with?("D")
+      puts "#{(index + 1)}. #{name[:name]} (#{name[:cohort]} cohort)"
+    end
+  end
+end
+
+def print_footer(names)# finally, we print the total
+puts "Overall, we have #{names.count} great students"
+end
+
+# nothing happens until we call the methods
+students = input_students
+print_header
+print(students)
+print_footer(students)
