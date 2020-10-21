5bb066226e52466b47bce18847f9b364cefa054f
Lucy Stringer <lucyjstringer@gmail.com>
2020-10-21 14:36:24 +0100

    add hard-coded hobby & origin to student hashes

--- directory_copy.rb
+++ directory_copy.rb
@@ -8,7 +8,7 @@ def input_students
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student has to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: :november, hobby: :crime, origin: :England}
     puts "Now we have #{students.count} students"
     # get another name from the user
     name = gets.chomp
