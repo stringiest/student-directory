53683d653d4edfa2df7b0faa90f5f5b3a72bd742
Lucy Stringer <lucyjstringer@gmail.com>
2020-10-22 11:49:46 +0100

    refactored to create clear methods for each task

--- directory.rb
+++ directory.rb
@@ -1,32 +1,43 @@
+@students = []
+
 def interactive_menu
   loop do
     # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
+    print_menu
     # 2. read the input and save it into a variable
-    selection = gets.chomp
     # 3. do what the user has asked
-    case selection
+    process(gets.chomp)
+  end
+end
+
+def process(selection)
+  case selection
     when "1"
       students = input_students
     when "2"
-      print_header
-      print(@students)
-      print_footer(@students)
+      show_students
     when "9"
       exit # this will cause the program to terminate
     else
       puts "I don't know what you meant, try again"
-    end
   end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
 end
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  @students = []
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
@@ -37,8 +48,6 @@ def input_students
     # get another name from the user
     name = gets.chomp
   end
-  # return the array of students
-  @students
 end
 
 def print_header
@@ -46,14 +55,14 @@ def print_header
   puts "-------------"
 end
 
-def print(names)
-  names.each do |name|
+def print_students_list
+  @students.each do |name|
     puts "#{name[:name]} (#{name[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)# finally, we print the total
-puts "Overall, we have #{names.count} great students"
+def print_footer# finally, we print the total
+puts "Overall, we have #{@students.count} great students"
 end
 
 # nothing happens until we call the methods
